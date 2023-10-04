b39c4979d2b3df9df89f3c8116b9bd15e1fa9d11
DEPED <ecelis@gbox.adnu.edu.ph>
2023-10-04 21:30:53 +0800

    SignUp with contents

--- lib/features/signup_page.dart
+++ lib/features/signup_page.dart
@@ -1,16 +1,93 @@
 import 'package:flutter/material.dart';
+import 'splash_page.dart';
+
 
 class SignUpPage extends StatelessWidget{
   const SignUpPage({super.key});
   
   @override
   Widget build(BuildContext context) {
-   return const Center(
-    child: Text('Sign Up', 
-    style:TextStyle(
-      fontSize: 30,
+   return  Scaffold(
+    body: Padding(
+       padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: Column(
+                children: [
+                  Text(
+                    'Hello, Mare!',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF276A7B),
+                    ),
+                  ),
+                  Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF276A7B),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height:20),
+             SizedBox(
+              height: 100,
+              width: 100,
+              child: Center(
+                child: Image.asset(
+                  'assets/Logo.png',
+                  // fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SizedBox(height:100),
+             TextField(
+                decoration: InputDecoration(
+                  labelText: 'FullName',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height:10),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.emailAddress,
+              ),
+              SizedBox(height:10),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+              ),
+              SizedBox(height:40),
+              RegButton(
+              width: 400,
+              height: 40,
+              text: 'Sign Up',
+              textColor: Colors.white,
+              bgColor: const Color(0xFF276A7B),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SignUpPage()),
+                );},
+            ),
+          ],
     ),
-    ),
-    );
+   ), 
+   )
+   ;
 }
 }
